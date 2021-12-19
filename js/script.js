75558df6c41dac2c4ac7d39d89f30ee08e258361
iampratiktandel <iampratiktandel@gmail.com>
2021-12-19 15:31:57 +0530

    feat: smooth-scrolling

--- js/script.js
+++ js/script.js
@@ -8,3 +8,33 @@ const headerEl = document.querySelector('.header');
 btnNavEl.addEventListener('click', () => {
   headerEl.classList.toggle('nav-open');
 })
+
+/** Smooth Scrolling */
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach((element) => {
+  element.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const href = element.getAttribute('href');
+
+    //  scroll back to top
+    if (href === '#') {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    }
+
+    // scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior: "smooth"});
+    }
+
+    // close mobile navigation
+    if (element.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  })
+
+})
